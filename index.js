a7f5a77812c2567a03fe04fff69f4f4f783c4174
JuanValentin <juanvalentinmejia@gmail>
2024-08-24 20:14:19 -0500

    Add Express server for Render deployment"

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 require('dotenv').config();
 
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bot is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 const Discord = require('discord.js');
 //const config = require('./config/config.json');
 const token = process.env.DISCORD_TOKEN;
