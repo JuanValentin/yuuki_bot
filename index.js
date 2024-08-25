10e20a7f7febfade1659614bd5d13fb0473f7bd2
JuanValentin <juanvalentinmejia@gmail>
2024-08-24 19:17:20 -0500

    Yuuiki bot de Princess Connet Re Dive v1

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+//const config = require('./config/config.json');
+const token = process.env.DISCORD_TOKEN;
+const prefix = process.env.PREFIX;
+
+const fs = require('fs');
+require('colors')
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMembers,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildEmojisAndStickers,
+    Discord.GatewayIntentBits.GuildMessageReactions,
+    Discord.GatewayIntentBits.MessageContent,
+  ],
+  partials:[Discord.Partials.Message, Discord.Partials.Channel, Discord.Partials.Reaction, Discord.Partials.GuildMember, Discord.Partials.User],
+})
+
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.slashcommands = new Discord.Collection();
+fs.readdirSync('./handlers').filter(handler => handler.endsWith(".js")).forEach(handler => {
+  try {
+    require(`./handlers/${handler}`)(client, Discord)
+  } catch (e) {
+    console.warn(e);
+  }
+})
+
+client.login(token);
